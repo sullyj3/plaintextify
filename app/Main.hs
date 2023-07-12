b1ae8e0b5c9d3b9796b4440a74106a894787a670
James Sully <sullyj3@gmail.com>
2023-07-12 12:52:58 +1000

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -43,60 +43,66 @@ main = do
   let nToFetch = length urls
   nFetchedVar <- newMVar 0
 
-  T.hPutStrLn stderr $ "fetching..."
-  for_ urls \url -> T.hPutStrLn stderr $ "  " <> url
-
-
-  let showNFetched :: Int -> Text
-      showNFetched n = "( " <> T.pack (show n) <> "/" <> T.pack (show nToFetch) <> " )"
-  T.hPutStrLn stderr $ showNFetched 0
-
   -- create lock for stdout in order to prevent interleaved output
   stdoutLock <- newMVar ()
 
+  let showNFetched :: Int -> Text
+      showNFetched n = "( " <> T.pack (show n) <> "/" <> T.pack (show nToFetch) <> " )"
+
+      fetchPage :: Text -> IO (Page LBS.ByteString)
+      fetchPage url = do
+        response <- get $ T.unpack url
+
+        -- expect html
+        let content_type :: ByteString
+            content_type = response ^. responseHeader "Content-Type"
+        case BS.take 9 content_type of
+          "text/html" -> pure ()
+          _ -> error $ "expected html, got " <> show content_type
+
+        let lbody = response ^. responseBody
+        nFetched <- modifyMVar nFetchedVar \n -> pure (n+1, n+1)
+        withMVar stdoutLock \_ -> do
+          hCursorUpLine stderr 1
+          hClearLine stderr
+          T.hPutStrLn stderr $ showNFetched nFetched
+
+        pure $ Page url lbody
+
+      toPlainTextPage :: Page LBS.ByteString -> IO (Page Text)
+      toPlainTextPage (Page url lbody) = do
+        -- detect charset and convert to unicode text. We want to be lenient here 
+        -- to support older websites.
+        let strictBody = BS.toStrict lbody
+        bodyText <- case map toLower <$> HtmlEncoding.detect lbody of
+          Just "utf-8" -> pure $ Enc.decodeUtf8Lenient strictBody
+          Just "ascii" -> pure $ Enc.decodeUtf8Lenient strictBody
+          Just "iso-8859-1" -> pure $ Enc.decodeLatin1 strictBody
+          Just "windows-1252" -> do
+            converter <- ICUConvert.open "CP1252" Nothing
+            pure $ ICUConvert.toUnicode converter strictBody
+          Just charset -> error $ "unsupported charset " <> show charset
+          Nothing -> error $ "could not detect charset of url " <> T.unpack url
+            
+        -- convert html to plain text
+        Pandoc.runIOorExplode $ do
+          pandoc <- Pandoc.readHtml Pandoc.def bodyText
+          plain <- Pandoc.writePlain Pandoc.def pandoc
+          pure $ Page url plain
+
+
+  T.hPutStrLn stderr $ "fetching..."
+  for_ urls \url -> T.hPutStrLn stderr $ "  " <> url
+  T.hPutStrLn stderr $ showNFetched 0
+
   -- fetch urls concurrently
-  (pages :: [Page LBS.ByteString]) <- forConcurrently urls $ \url -> do
-    response <- get $ T.unpack url
-
-    -- expect html
-    let content_type :: ByteString
-        content_type = response ^. responseHeader "Content-Type"
-    case BS.take 9 content_type of
-      "text/html" -> pure ()
-      _ -> error $ "expected html, got " <> show content_type
-
-    let lbody = response ^. responseBody
-    nFetched <- modifyMVar nFetchedVar \n -> pure (n+1, n+1)
-    withMVar stdoutLock \_ -> do
-      hCursorUpLine stderr 1
-      hClearLine stderr
-      T.hPutStrLn stderr $ showNFetched nFetched
-
-    pure $ Page url lbody
+  (pages :: [Page LBS.ByteString]) <- mapConcurrently fetchPage urls
 
   T.hPutStrLn stderr $ "done."
   T.hPutStrLn stderr $ ""
 
   -- concurrently convert each html page to plain text
-  (plainPages :: [Page Text]) <- forConcurrently pages $ \(Page url lbody) -> do
-    -- detect charset and convert to unicode text. We want to be lenient here 
-    -- to support older websites.
-    let strictBody = BS.toStrict lbody
-    bodyText <- case map toLower <$> HtmlEncoding.detect lbody of
-      Just "utf-8" -> pure $ Enc.decodeUtf8Lenient strictBody
-      Just "ascii" -> pure $ Enc.decodeUtf8Lenient strictBody
-      Just "iso-8859-1" -> pure $ Enc.decodeLatin1 strictBody
-      Just "windows-1252" -> do
-        converter <- ICUConvert.open "CP1252" Nothing
-        pure $ ICUConvert.toUnicode converter strictBody
-      Just charset -> error $ "unsupported charset " <> show charset
-      Nothing -> error $ "could not detect charset of url " <> T.unpack url
-        
-    -- convert html to plain text
-    Pandoc.runIOorExplode $ do
-      pandoc <- Pandoc.readHtml Pandoc.def bodyText
-      plain <- Pandoc.writePlain Pandoc.def pandoc
-      pure $ Page url plain
+  (plainPages :: [Page Text]) <- mapConcurrently toPlainTextPage pages
 
   case mode of
     OutputStdout -> do
